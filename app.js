0f0822bceaf4266149155327aa13600e76181ace
plagiarism2012 <sonu.deo2012@gmail.com>
2024-07-20 07:56:07 +0530

    Deploy check

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+var cors = require("cors");
+const bodyParser = require('body-parser');
+const postRoute = require('./routes/book_routes');
+const mongoose = require('mongoose');
+require('dotenv/config');
+const usersRoute = require('./routes/LibrarianRoute');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+var whitelist = [ "https://master--athenaeum-the-library.netlify.app"] 
+const corsOptions = { origin: (origin, callback) => { if(!origin || whitelist.includes(origin)) { callback(null, true) } else { callback(new Error('Not allowed by CORS')) } }, credentials: true, origin: true } 
+app.use(cors(corsOptions));
+//--------------------------------------------------------------
+//Book Database
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
+
+//---------------------------------------------------------------
+//User Database
+
+mongoose.connect(process.env.dbUrl, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+  .then(() => console.log("DB conncected"))
+  .catch(err => console.log(err));
+
+
+//----------------------------------------------------------------
+
+app.use(bodyParser.json());
+
+app.use('/', postRoute);
+app.use('/user', usersRoute);
+
+app.get('/', (req, res) => {
+  res.send('we are on home');
+})
+
+app.listen("https://ath-mnab.onrender.com" | 5500);
