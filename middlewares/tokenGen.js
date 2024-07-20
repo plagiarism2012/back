bf31272b437bc705d7e3e65b2ba1da4eecfaecdc
Sonu kumar deo <81760592+plagiarism2012@users.noreply.github.com>
2024-07-20 07:43:48 +0530

    Delete middlewares directory

--- middlewares/tokenGen.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-//assigning token to a user based on userId so that only authorised users can fetch/update books
-const generateToken = (userID) => {
-    return jwt.sign({ id: userID }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "1d",
-    });
-}
-module.exports = generateToken;
